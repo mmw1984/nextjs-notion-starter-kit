e5816dbaa6b1419ac9f8aeac30542457edcde13f
Wong Tsz Fung <tszfeng43@gmail.com>
2025-06-16 15:51:44 +0800

    Added Instagram

--- lib/config.ts
+++ lib/config.ts
@@ -61,6 +61,7 @@ export const mastodon: string | undefined = getSiteConfig('mastodon')
 export const github: string | undefined = getSiteConfig('github')
 export const youtube: string | undefined = getSiteConfig('youtube')
 export const linkedin: string | undefined = getSiteConfig('linkedin')
+export const instagram: string | undefined = getSiteConfig('instagram')
 export const newsletter: string | undefined = getSiteConfig('newsletter')
 export const zhihu: string | undefined = getSiteConfig('zhihu')
 
